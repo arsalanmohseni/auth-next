e9e5ae8b5a1aced1db5b546cd09614b13ef7ca69
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-08-26 11:54:22 +0330

    Installed new dependencies, updated packages, added format script to package.json, formated codes with prettier

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (
-      <div className='bg-white dark:bg-gray-900'>
-          <h1 className='text-black dark:text-white text-6xl max-sm:text-3xl max-md:text-4xl'>Sign in</h1>
-    </div>
-  )
-}
+    return (
+        <div className="bg-white dark:bg-gray-900">
+            <h1 className="text-6xl text-black max-md:text-4xl max-sm:text-3xl dark:text-white">
+                Sign in
+            </h1>
+        </div>
+    );
+};
 
-export default page
+export default page;
