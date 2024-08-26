153b7b37b979818d66307d0988e766e007bec9f4
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-08-26 11:30:24 +0330

    Created auth page file

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+      <div className='bg-white dark:bg-gray-900'>
+          <h1 className='text-black dark:text-white text-6xl max-sm:text-3xl max-md:text-4xl'>Sign in</h1>
+    </div>
+  )
+}
+
+export default page
